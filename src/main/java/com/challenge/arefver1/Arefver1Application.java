0ce4351d726b58395671a77c61160fae58a4a44c
Franco Batista <francobatista09@gmail.com>
2024-02-26 19:38:21 -0300

    commit 1

---
+++ src/main/java/com/challenge/arefver1/Arefver1Application.java
@@ -0,0 +1,13 @@
+package com.challenge.arefver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arefver1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arefver1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/arefver1/Arefver1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.arefver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arefver1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
